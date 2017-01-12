2e0baead18f7773ddcc2330ea274a401fe6e2f1d
Venkata Karthik Paruchuri <paruchuri@zoobe.com>
2017-01-12 13:25:17 +0100

    Application file added.

---
+++ Android/app/src/main/java/com/karcompany/imageexplorer/ImageExplorerApplication.java
@@ -0,0 +1,18 @@
+package com.karcompany.imageexplorer;
+
+/**
+ * Created by pvkarthik on 2017-01-12.
+ *
+ * Android Application.
+ */
+
+import android.app.Application;
+
+public class ImageExplorerApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
+
